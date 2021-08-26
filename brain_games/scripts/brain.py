30cad9f1fe04066d511dfda3c8be7b49aed39410
Pavel <variagkzn@mail.ru>
2021-08-26 14:12:20 +0300

    update7

--- brain_games/scripts/brain.py
+++ brain_games/scripts/brain.py
@@ -4,7 +4,7 @@ import prompt
 
 def name_user():
     name_us = prompt.string('May I have your name? ')
-    print('Hello, {0}'.format(name_us))
+    print('Hello, {0}!'.format(name_us))
     return name_us
 
 
